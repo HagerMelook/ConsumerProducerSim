08c15212eb7450315c516f46c71e04159bc746d7
RanaMohamed6720 <121067602+RanaMohamed6720@users.noreply.github.com>
2024-01-19 17:12:26 +0200

    Update ShapeFactory.js

--- front-end/src/Shapes/ShapeFactory.js
+++ front-end/src/Shapes/ShapeFactory.js
@@ -1,13 +1,16 @@
-
 import Queue from "@/Shapes/Queue";
 import Machine from "@/Shapes/Machine";
 
 export default class ShapeFactory {
   static createQueue(number, draggable) {
-    return new Queue(number, draggable);
+    const queue = new Queue(number, draggable);
+    queue.id = `Q${number}`;
+    return queue;
   }
 
   static createMachine(number, draggable) {
-    return new Machine(number, draggable);
+    const machine = new Machine(number, draggable);
+    machine.id = `M${number}`;
+    return machine;
   }
 }
